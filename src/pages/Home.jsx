4dc050ed737e6fce1ea6088543816491ec7a58d3
HiiTuongSing <117565834+HiiTuongSing@users.noreply.github.com>
2024-05-01 14:12:54 +0800

    Added Images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Cat from "/src/images/cat.jpg";
 
 export default function Home() {
   return (
     <div>
       <h1>This is the homepage component!</h1>
+      <img width="200" height="200" src={Cat} alt="cat" className="rounded" />
       <Link className="btn btn-outline-primary" to="/react-github-page/page1">
         Page 1
       </Link>
--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Cat from "/src/images/cat2.jpg";
 
 export default function Page1() {
   return (
     <div>
       <h1>This is page 1!</h1>
+      <img width="200" height="200" src={Cat} alt="cat" className="rounded" />
       <Link className="btn btn-outline-primary" to="/react-github-page/">
         Home
       </Link>
